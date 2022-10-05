95a3a43f427d4f5373d7ddedf00c6320206ada6a
jsebasv2501 <juan.vallecilla.mt@usa.edu.co>
2022-10-04 19:00:14 -0500

    adicion reto 3

---
+++ src/main/java/com/example/reto3/reto3/model/Clientes.java
@@ -0,0 +1,52 @@
+package com.example.reto3.reto3.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Clientes {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCliente;
+    private String name;
+    private String email;
+    private Integer age;
+    private String password;
+
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
